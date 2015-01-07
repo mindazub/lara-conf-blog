d356084a63bc2dfa81e341afd9c814243c851754
mindazub <mindaugas.azubalis@gmail.com>
2015-01-07 21:39:28 +0200

    Update mail.php

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'mindaugas.azubalis@gmail.com', 'name' => 'Mindaugas Azubalis'),
+	'from' => array('address' => 'you@gmail.com', 'name' => 'your name'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => 'mindaugas.azubalis@gmail.com',
+	'username' => 'email',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'generation123',
+	'password' => 'pasw',
 
 	/*
 	|--------------------------------------------------------------------------
