6c113070625062aad56835708e71ada022369b72
mindazub <mindaugas.azubalis@gmail.com>
2015-01-12 17:36:15 +0200

    shopas pajudintas

--- app/views/shop/index.blade.php
+++ app/views/shop/index.blade.php
@@ -8,29 +8,105 @@
 <div class="row">
 
 		<div class="col-md-12">
-				<div class="col-md-4">Categories</div>
+				<div class="col-md-4">
+					<p>Categories</p>
+						<ul class="list-group">
+						  <li class="list-group-item">Laptops</li>
+						  <li class="list-group-item">Desktop</li>
+						  <li class="list-group-item">Smartphones</li>
+						  <li class="list-group-item">Tablets</li>
+						  <li class="list-group-item">Headphones</li>
+						</ul>
+
+
+
+				</div>
 
 				<div class="col-md-8">
-
 					<div class="row">
-							<div class="col-md-4">Product nr 1</div>
+							<div class="col-md-4">
+									<div class="row">
+										<img src="img/shop/300_300_1.jpg" alt="youtube">
+									</div>
+									<h3><a href="#">Product No 1</a></h3>
+									<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.</small>
+									<p>...</p>
+							        <p>
+							        	<a href="#" class="btn btn-primary" role="button">Buy</a>
+								        <a href="#" class="btn btn-default" role="button">Description</a>
+							        </p>
+							</div>
+							<div class="col-md-4">
+									<div class="row">
+										<img src="img/shop/300_300_2.jpg" alt="youtube">
+									</div>
+									<h3><a href="#">Product No 2</a></h3>
+									<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.</small>
+									<p>...</p>
+							        <p>
+							        	<a href="#" class="btn btn-primary" role="button">Buy</a>
+								        <a href="#" class="btn btn-default" role="button">Description</a>
+							        </p>
+							</div>
+							<div class="col-md-4">
+									<div class="row">
+										<img src="img/shop/300_300_3.jpg" alt="youtube">
+									</div>
+									<h3><a href="#">Product No 3</a></h3>
+									<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.</small>
+									<p>...</p>
+							        <p>
+							        	<a href="#" class="btn btn-primary" role="button">Buy</a>
+								        <a href="#" class="btn btn-default" role="button">Description</a>
+							        </p>
+							</div>							
+					</div> <!-- row -->
 
-							<div class="col-md-4">Product nr 2</div>
 
-							<div class="col-md-4">Product nr 3</div>
-
-							<!-- <div class="col-md-4">Product nr 4</div> -->
-					</div>
-
-					<div class="row">
-							<div class="col-md-4">Product nr 1</div>
-
-							<div class="col-md-4">Product nr 2</div>
-
-							<div class="col-md-4">Product nr 3</div>
-
-							<!-- <div class="col-md-4">Product nr 4</div> -->
-					</div>
+<div class="row">
+							<div class="col-md-4">
+									<div class="row">
+										<img src="img/shop/300_300_1.jpg" alt="youtube">
+									</div>
+									<h3><a href="#">Product No 1</a></h3>
+									<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.</small>
+									<p>...</p>
+							        <p>
+							        	<a href="#" class="btn btn-primary" role="button">Buy</a>
+								        <a href="#" class="btn btn-default" role="button">Description</a>
+							        </p>
+							</div>
+							<div class="col-md-4">
+									<div class="row">
+										<img src="img/shop/300_300_2.jpg" alt="youtube">
+									</div>
+									<h3><a href="#">Product No 2</a></h3>
+									<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.</small>
+									<p>...</p>
+							        <p>
+							        	<a href="#" class="btn btn-primary" role="button">Buy</a>
+								        <a href="#" class="btn btn-default" role="button">Description</a>
+							        </p>
+							</div>
+							<div class="col-md-4">
+									<div class="row">										
+										<img src="img/shop/300_300_3.jpg" alt="youtube">
+									</div>
+									<h3><a href="#">Product No 3</a></h3>
+									<small>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.</small>
+									<p>...</p>
+							        <p>
+							        	<a href="#" class="btn btn-primary" role="button">Buy</a>
+								        <a href="#" class="btn btn-default" role="button">Description</a>
+							        </p>
+							</div>							
+					</div> <!-- row -->
 
 
 				</div>
